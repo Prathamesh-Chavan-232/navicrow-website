e6107736988e8da16e70d513a1bb365b8936dcd2
falcon_codes <prathamesh.chavanpsc2018@gmail.com>
2023-05-03 19:50:24 +0530

    init - navicrow website (2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,13 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		screens: {
-			sm: "480px",
-			md: "768px",
-			lg: "976px",
-			xl: "1440pxs",
-		},
 		extend: {
-			colors: {},
+			screens: {
+				sm: "480px",
+				md: "768px",
+				lg: "976px",
+				xl: "1440pxs",
+			},
 		},
 	},
 	plugins: [],
