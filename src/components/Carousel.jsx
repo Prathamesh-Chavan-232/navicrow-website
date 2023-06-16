6096f67ef7e30a7de36dbabdd01f117a87e9fdb1
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-06-16 19:47:42 +0530

    feat: Slider to Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import dest1 from "../assets/landscapes/destinations-1.jpg";
 import dest2 from "../assets/landscapes/dodham.png";
 import dest3 from "../assets/landscapes/mountains-5.jpg";
@@ -7,66 +8,56 @@ const carousel = [
 	{
 		title: "Char Dham",
 		img: dest1,
+		link: "/destinations/char-dham",
 	},
 	{
 		title: "Do Dham",
 		img: dest2,
+		link: "/destinations/do-dham",
 	},
 	{
 		title: "Kedarnath",
 		img: dest3,
+		link: "/destinations/kedarnath",
 	},
 ];
 
-const Img = ({ img, className }) => {
-	return (
-		<button>
-			<img
-				src={img}
-				alt="arrow"
-				className={"absolute top-0 m-auto w-[10%] h-[20%] " + (className || "")}
-			/>
-		</button>
-	);
-};
-
-function slideLeft() {
-	var slider = document.getElementById("slider");
-	slider.scrollLeft = slider.scrollLeft - 100;
-}
-
-function slideRight() {
-	var slider = document.getElementById("slider");
-	slider.scrollLeft = slider.scrollLeft + 1080;
-}
-
 export const Carousel = () => {
+	const [index, setIndex] = useState(0);
 	return (
-		<div className="relative flex items-center bg-black">
-			<MdChevronLeft onClick={slideLeft} size={80} />
-			<div
-				id="slider"
-				className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth"
+		<div className="relative h-[400px] lg:h-screen bg-light-gray">
+			<button
+				className="absolute top-1/2 left-10"
+				onClick={() => {
+					if (index !== 0) setIndex(index - 1);
+				}}
 			>
-				{carousel.map((item, i) => {
-					return (
-						<div key={i} className="relative inline-block">
-							<img
-								src={item.img}
-								alt="carousel image"
-								className="w-screen h-screen object-cover"
-							/>
-							<div className=" text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-								<h1 className="text-4xl lg:text-6xl font-fancy">
-									{item.title}
-								</h1>
-								<button></button>
-							</div>
-						</div>
-					);
-				})}
+				<MdChevronLeft size={60} />
+			</button>
+
+			<div id="slider" className="">
+				<div
+					className="w-screen h-[400px] lg:h-screen object-cover duration-500 bg-center bg-cover bg-no-repeat "
+					style={{ backgroundImage: `url(${carousel[index].img})` }}
+				>
+					<div className="flex flex-col items-center justify-center gap-4 text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+						<h1 className="text-4xl lg:text-6xl font-fancy">
+							{carousel[index].title}
+						</h1>
+						<button className="px-10 py-4 border border-white bg-transparent text-2xl text-white rounded-full baseline transition-color duration-200 hover:text-olive-green hover:bg-white">
+							<a href={carousel[index].link}>Discover more</a>
+						</button>
+					</div>
+				</div>
 			</div>
-			<MdChevronRight onClick={slideRight} size={80} />
+			<button
+				onClick={() => {
+					if (index !== carousel.length - 1) setIndex(index + 1);
+				}}
+				className="absolute top-1/2 right-10"
+			>
+				<MdChevronRight size={60} />
+			</button>
 		</div>
 	);
 };
