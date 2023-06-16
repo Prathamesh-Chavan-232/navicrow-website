985c6be19242f29d37844c2041f10d9ca53c4416
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-06-16 19:12:34 +0530

    feat: Temp Slider

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,27 +1,7 @@
-import arrowLeft from "../assets/arrowl.png";
-import arrowRight from "../assets/arrowr.png";
-import carousel1 from "../assets/landscapes/carousel-1.jpeg";
-import carousel2 from "../assets/landscapes/carousel-2.jpeg";
-import carousel3 from "../assets/landscapes/carousel-3.jpeg";
 import dest1 from "../assets/landscapes/destinations-1.jpg";
 import dest2 from "../assets/landscapes/dodham.png";
 import dest3 from "../assets/landscapes/mountains-5.jpg";
-import { useState } from "react";
-
-const destinationsSlider = [
-	{
-		title: "Char Dham",
-		img: carousel1,
-	},
-	{
-		title: "Do Dham",
-		img: carousel2,
-	},
-	{
-		title: "Kedarnath",
-		img: carousel3,
-	},
-];
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const carousel = [
 	{
@@ -44,19 +24,26 @@ const Img = ({ img, className }) => {
 			<img
 				src={img}
 				alt="arrow"
-				className={
-					"absolute top-0 bottom-0 m-auto w-[10%] h-[20%] " + (className || "")
-				}
+				className={"absolute top-0 m-auto w-[10%] h-[20%] " + (className || "")}
 			/>
 		</button>
 	);
 };
 
+function slideLeft() {
+	var slider = document.getElementById("slider");
+	slider.scrollLeft = slider.scrollLeft - 100;
+}
+
+function slideRight() {
+	var slider = document.getElementById("slider");
+	slider.scrollLeft = slider.scrollLeft + 1080;
+}
+
 export const Carousel = () => {
-	const [index, setIndex] = useState(0);
 	return (
-		<div className="relative flex items-center">
-			<Img img={arrowRight} className={"right-10"} />
+		<div className="relative flex items-center bg-black">
+			<MdChevronLeft onClick={slideLeft} size={80} />
 			<div
 				id="slider"
 				className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth"
@@ -79,6 +66,7 @@ export const Carousel = () => {
 					);
 				})}
 			</div>
+			<MdChevronRight onClick={slideRight} size={80} />
 		</div>
 	);
 };
