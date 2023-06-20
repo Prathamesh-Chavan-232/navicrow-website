ccdb34cbe597309fdb90484ee67f7ccb3f85db7f
K-Cloud25 <keyuroak@gmail.com>
2023-06-20 17:05:59 +0530

    updated Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -42,14 +42,51 @@ const Arrow = ({ onClick, className, children }) => {
 
 export const Carousel = () => {
 	const [index, setIndex] = useState(0);
+	const [[page, direction], setSlide] = useState([0,0]);
+
+	const paginate = (newDirection) => {
+		setSlide([page + newDirection, newDirection]);
+	  };
+	
 
 	const slideLeft = () => {
-		if (index !== 0) setIndex(index - 1);
+		if (index !== 0){ 
+			setIndex(index - 1);
+			paginate(1)
+		}
 	};
 
 	const slideRight = () => {
-		if (index !== carousel.length - 1) setIndex(index + 1);
+		if (index !== carousel.length - 1) 
+		{setIndex(index + 1);
+		paginate(-1);
+	}
 	};
+
+	const variants = {
+		enter: (direction) => {
+			console.log(direction);
+		  return {
+			// x: direction > 0 ? 1000 : -1000,
+			clipPath: direction > 0 ? "circle(10% at 0% 50%)" : "circle(10% at 100% 50%)",
+			opacity: 0.3
+		  };
+		},
+		center: {
+		  zIndex: 3,
+		//   x: 0,
+			clipPath: direction > 0 ? "circle(150% at 0% 50%)" : "circle(150% at 100% 50%)",
+		  opacity: 1
+		},
+		exit: (direction) => {
+		  return {
+			zIndex: 0,
+			// x: direction < 0 ? 1000 : -1000,
+			clipPath: direction > 0 ? "circle(10% at 100% 50%)" : "circle(10% at 0% 50%)",
+			opacity: 0.3
+		  };
+		}
+	  };
 
 	return (
 		<div className="relative h-[400px] lg:h-screen bg-light-gray group">
@@ -57,17 +94,25 @@ export const Carousel = () => {
 				<BsChevronCompactLeft />
 			</Arrow>
 
-				<AnimatePresence>
-					<div className="primary-header">
-						{index === 0 && (
+				<div className="primary-header">
+					<AnimatePresence initial={false} custom={direction} >
 							<motion.div
-								initial={{clipPath:"circle(0% at 100% 50%)", zIndex:3}}
-								animate={{clipPath:"circle(150% at 100% 50%)", zIndex:3}}
-								exit={{clipPath:"circle(0% at 0% 50%)",zIndex:3}}
+								// initial={{clipPath:"circle(0% at 0% 50%)", zIndex:3}}
+								// animate={{clipPath:"circle(150% at 0% 50%)", zIndex:3}}
+								// exit={{clipPath:"circle(0% at 100% 50%)",zIndex:3}}
 
+								custom={direction}
+								variants={variants}
+								initial="enter"
+								animate="center"
+								exit="exit"
+								transition={{
+									opacity: { duration: 0.2 }
+								}}
 								key={"Carousel-item-holder-"+index}
+
 								className="customId_obj w-screen h-[400px] lg:h-screen object-cover duration-500 bg-center bg-cover bg-no-repeat "
-								style={{ backgroundImage: `url(${carousel[0].img})` }}>
+								style={{ backgroundImage: `url(${carousel[index].img})` }}>
 								<div className="flex flex-col items-center justify-center gap-4 text-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
 									<motion.h1
 									initial={{opacity:0}}
@@ -76,23 +121,23 @@ export const Carousel = () => {
 									transition={{duration:0.6}} 
 									key={"Carousel-item-header-text-holder-"+index}
 									className="text-4xl lg:text-6xl font-fancy">
-										{carousel[0].title}
+										{carousel[index].title}
 									</motion.h1>
 									<a
 										key={"Carousel-item-linked-text-holder-"+index}
-										href={carousel[0].link}
+										href={carousel[index].link}
 										className="px-10 py-4 border border-white bg-transparent text-md lg:text-2xl text-white rounded-full baseline transition-color duration-200 hover:text-olive-green hover:bg-white"
 									>
 										Discover more
 									</a>
 								</div>
 							</motion.div>
-						)}
-						{index === 1 && (
+						
+						{/* {index === 1 && (
 							<motion.div
-								initial={{clipPath:"circle(0% at 100% 50%)", zIndex:3}}
-								animate={{clipPath:"circle(150% at 100% 50%)", zIndex:3}}
-								exit={{clipPath:"circle(0% at 0% 50%)",zIndex:3}}
+								initial={{clipPath:"circle(0% at 0% 50%)", zIndex:3}}
+								animate={{clipPath:"circle(150% at 0% 50%)", zIndex:3}}
+								exit={{clipPath:"circle(0% at 100% 50%)",zIndex:3}}
 
 							key={"Carousel-item-holder-"+index}
 								className="customId_obj w-screen h-[400px] lg:h-screen object-cover duration-500 bg-center bg-cover bg-no-repeat "
@@ -120,9 +165,9 @@ export const Carousel = () => {
 						)}
 						{index === 2 && (
 							<motion.div
-								initial={{clipPath:"circle(0% at 100% 50%)", zIndex:3}}
-								animate={{clipPath:"circle(150% at 100% 50%)", zIndex:3}}
-								exit={{clipPath:"circle(0% at 0% 50%)",zIndex:3}}
+								initial={{clipPath:"circle(0% at 0% 50%)", zIndex:3}}
+								animate={{clipPath:"circle(150% at 0% 50%)", zIndex:3}}
+								exit={{clipPath:"circle(0% at 100% 50%)",zIndex:3}}
 
 							key={"Carousel-item-holder-"+index}
 								className="customId_obj w-screen h-[400px] lg:h-screen object-cover duration-500 bg-center bg-cover bg-no-repeat "
@@ -146,9 +191,9 @@ export const Carousel = () => {
 									</a>
 								</div>
 							</motion.div>
-						)}
-					</div>
-				</AnimatePresence>
+						)} */}
+					</AnimatePresence>
+				</div>
 				
 			<Arrow onClick={slideRight} className={"z-[50] right-2 lg:right-10"}>
 				<BsChevronCompactRight />
